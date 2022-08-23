fba4a67c30fdb8b7350dd9e703f56c51af0c1895
LeeWei <meetleev@gmail.com>
2022-08-23 20:07:11 +0800

    init commit

--- dart/gen_protoc_dart.py
+++ dart/gen_protoc_dart.py
@@ -16,7 +16,7 @@ def check_environment_variables():
 def protos_to_dart(executables, srcDir, destDir):
     check_environment_variables()
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    protoc_gen_dart_bin = os.path.join(current_dir, 'protobuf.dart-protobuf-v2.1.0/protoc_plugin/bin/')
+    protoc_gen_dart_bin = os.path.join(current_dir, 'protobuf.dart-protobuf/protoc_plugin/bin/')
     sysPlat = platform.system()
     protoc_gen_dart = os.path.join(protoc_gen_dart_bin, 'Darwin' == sysPlat and 'protoc-gen-dart' or 'protoc-gen-dart.bat')
     sCmd = "%s --proto_path=%s --dart_out=%s %s/*.proto --plugin=%s" % (executables, srcDir, destDir, srcDir, protoc_gen_dart)
