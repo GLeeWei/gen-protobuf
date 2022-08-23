bc0e40f7235b860e39348fa7663551172e0b1d4d
LeeWei <meetleev@gmail.com>
2022-08-23 20:23:19 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gen-protobuf
+
+This can be used to generate static code for google protobuf.
+
+
+```
+  usage: python gen-protoc.py [options]
+
+  -s, --src    	  	proto source dir.
+
+  -d, --dest      	destination dir.
+
+  -l, --lanuage   	complie target lanuage, support js, ts,  dart, cpp, objc, csharp, java, kotlin, python, ruby, php
+
+  -bundle      		bundle name, only support ts or js, deafult: protos
+```
--- gen-protoc.py
+++ gen-protoc.py
@@ -19,28 +19,28 @@ def main():
 	import argparse
 	usage = "python %prog -s/--src <source dir or single source file> -d/--dest <destination dir> --arr <out format is array>"
 	parser = argparse.ArgumentParser(usage)
-	parser.add_argument('-s', '--src', type=str, help='source dir or single source file', default=current_dir)
+	parser.add_argument('-s', '--src', type=str, help='proto source dir', default=current_dir)
 	parser.add_argument('-d', '--dest', type=str, help='destination dir', default=os.path.join(current_dir, 'protos'))
 	parser.add_argument('-bundle', type=str, help='bundle name, only support ts or js', default='protos')
-	parser.add_argument('-ext','--ext', help='out file ext, support js, ts,  dart, cpp, objc, csharp, java, kotlin, python, ruby, php', default='js')
+	parser.add_argument('-l','--lanuage', help='complie target lanuage, support js, ts,  dart, cpp, objc, csharp, java, kotlin, python, ruby, php', default='js')
 	args = parser.parse_args()
 
 	if os.path.isdir(args.dest):
 		shutil.rmtree(args.dest)
 	os.makedirs(args.dest)
 
-	if ('dart' == args.ext):
+	if ('dart' == args.lanuage):
 		protos_to_dart(getProtocExecutables(), args.src, args.dest)
-	elif ('js' == args.ext or 'ts' == args.ext):
-		protos_to_js(args.src, args.dest, args.bundle, 'ts' == args.ext)
-	elif ('lua' == args.ext):
+	elif ('js' == args.lanuage or 'ts' == args.lanuage):
+		protos_to_js(args.src, args.dest, args.bundle, 'ts' == args.lanuage)
+	elif ('lua' == args.lanuage):
 		print('unsupoort lua')
-	elif ('go' == args.ext):
+	elif ('go' == args.lanuage):
 		print('unsupoort go')
-	elif ('cpp' == args.ext or 'objc' == args.ext or 'csharp' == args.ext or
-		 'java' == args.ext or 'kotlin' == args.ext or 'python' == args.ext or
-		 'ruby' == args.ext or 'php' == args.ext):
-		protos_to_lanuage(args.src, args.dest, args.ext)
+	elif ('cpp' == args.lanuage or 'objc' == args.lanuage or 'csharp' == args.lanuage or
+		 'java' == args.lanuage or 'kotlin' == args.lanuage or 'python' == args.lanuage or
+		 'ruby' == args.lanuage or 'php' == args.lanuage):
+		protos_to_lanuage(args.src, args.dest, args.lanuage)
 
 if __name__=="__main__":
     main()
