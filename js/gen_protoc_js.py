3d7a5b633d1f23c93ee405387dafaa1b889f27a5
LeeWei <meetleev@gmail.com>
2022-08-28 11:43:33 +0800

    fix bugs

--- js/gen_protoc_js.py
+++ js/gen_protoc_js.py
@@ -34,7 +34,7 @@ def protos_to_js(srcDir, destDir, bundle, ts):
         bundleName = os.path.join(destDir, bundle)
         sProtoList = ' '.join(protoList)
         print('gen ' + sProtoList)
-        os.system("pbjs -t static-module  --no-convert --no-delimited --no-beautify  -w commonjs -o %s.js %s" % (bundleName, sProtoList))
+        os.system("pbjs -t static-module -w commonjs -o %s.js %s --no-beautify --no-typeurl " % (bundleName, sProtoList))
         ts and os.system("pbts -o %s.d.ts %s.js" % (bundleName, bundleName))
 
 
